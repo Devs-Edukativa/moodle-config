037f2bea7a35e71ad5483631be01767852fbb530
Rafael Dantas <rafaeldantasboeira@gmail.com>
2026-01-07 12:38:10 -0300

    Add comprehensive CSS styles for PNAE, AVSEC, and SESCINC modules
    - Introduced a new stylesheet for the PNAE module with responsive design elements, button styles, and card layouts.
    - Implemented specific styles for AVSEC and SESCINC modules, including button and card designs with hover effects.
    - Added color classes for emergency situations and specific module presentations.
    - Ensured consistent use of the Poppins font across all components.
    - Established grid layouts for buttons and modules to enhance responsiveness and user experience.

--- infraero/pnae-js.js
+++
@@ -1,6 +0,0 @@
-function handleConteudoCompleto() {
-  const container = document.querySelector(
-    '.container-conteudo-completo-pnae',
-  );
-  container.classList.toggle('show-conteudo-completo-pnae'); // Alterna a classe 'show'
-}
